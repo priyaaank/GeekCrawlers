b7cff13b7d56ef5903cfd436f06a7ceee0cac609
Priyank Gupta <priyaaank@gmail.com>
2012-01-12 00:27:24 +0530

    Adding capabilities to parse all the languages in github and then query all the associated users for that language.

--- adapters/github_crawler.rb
+++ adapters/github_crawler.rb
@@ -3,17 +3,50 @@ require 'mechanize'
 module Adapter
   class GithubCrawler
     
+    attr_accessor :language_listing_url, :agent, :languages, :search_url
+
     def initialize
-      
+      @language_listing_url = "http://github.com/languages"
+      @agent = Mechanize.new { |a| a.verify_mode = OpenSSL::SSL::VERIFY_NONE; a.user_agent_alias = "Mac Safari" }
+      @languages = []
+      @search_url = "https://github.com/search?langOverride=&language=&q=language%3A##LANGUAGE_NAME##&repo=&type=Users&x=19&y=21&start_value="
     end
 
     def crawl
-
+      populate_language_list
+      @languages.each do |language|
+        crawl_users_who_code_in language
+      end
     end
 
     private
 
+    def crawl_users_who_code_in language
+      puts "#{language} : #{page_count_for(language)}"
+    end
 
+    def populate_language_list
+      @languages = language_page.search(".right ul li a").collect do |language_url|
+        matched_language_name_from language_url
+      end
+    end
+
+    def matched_language_name_from language_url
+      (language_url.to_s.match(/<a href.*>(.*)<\/a>/) || [])[1]
+    end
+
+    def language_page
+      @agent.get(@language_listing_url)
+    end
+
+    def url_for language, page_number
+      (@search_url.gsub("##LANGUAGE_NAME##",language) + page_number.to_s)
+    end
+
+    def page_count_for language
+      node = @agent.get(url_for(language,1)).search(".pagination .pager_link:last-child").last 
+      (node.to_s.match(/<a href.*>(.*)<\/a>/)||[])[1]
+    end
 
   end
 end
--- run.rb
+++ run.rb
@@ -4,10 +4,12 @@ require './user'
 require './stackoverflow'
 require './crawler'
 require './adapters/stackoverflow_crawler'
+require './adapters/github_crawler'
 
 Mongoid.load!("./mongoid.yml")
 Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db("gc_dev")
 end
 
-Crawler.new([Adapter::StackoverflowCrawler.new]).process
+# Crawler.new([Adapter::StackoverflowCrawler.new]).process
+Crawler.new([Adapter::GithubCrawler.new]).process
